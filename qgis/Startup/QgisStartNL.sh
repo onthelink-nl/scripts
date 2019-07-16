7c20c73a3d023aab864108f159a25075017c13cb
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-07-16 20:51:42 +0200

    Update QgisStartNL.sh

--- qgis/Startup/QgisStartNL.sh
+++ qgis/Startup/QgisStartNL.sh
@@ -7,7 +7,7 @@ tput reset
 sudo rm -rf .qgis
 
 #Commando's voor het doorvoeren naar de Installatie
-mkdir .qgis && cd .qgis && sudo curl -LO https://github.com/onthelink-tk/scripts/blob/master/qgis/NL/QgisInstallerNL.OTL && tput reset && tput bold && tput setaf 5; echo Installatie bestanden gedownload! && tput setaf 3; echo Voer de verificatie code in over 3 seconden om verder te gaan && tput sgr 0 && sleep 3
+mkdir .qgis && cd .qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/NL/QgisInstallerNL.OTL && tput reset && tput bold && tput setaf 5; echo Installatie bestanden gedownload! && tput setaf 3; echo Voer de verificatie code in over 3 seconden om verder te gaan && tput sgr 0 && sleep 3
 
 #DIT IS HET DOORVERWIJSSCRIPT VOOR QGIS
 openssl enc -d -aes-256-cbc -a -in QgisInstallerNL.OTL | sudo bash -

ba6da838486d6d8cf997cffdd378f948077fc7cb
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-07-16 20:51:16 +0200

    Update QgisStartEN.sh

--- qgis/Startup/QgisStartEN.sh
+++ qgis/Startup/QgisStartEN.sh
@@ -7,7 +7,7 @@ tput reset
 sudo rm -rf .qgis
 
 #Download the script
-mkdir .qgis && cd .qgis && sudo curl -LO https://github.com/onthelink-tk/scripts/blob/master/qgis/EN/QgisInstallerEN.OTL && tput reset && tput bold && tput setaf 5; echo Installation files downloaded! && tput setaf 3; echo Please fill in the verification code in 3 seconds to execute the script && tput sgr 0 && sleep 3
+mkdir .qgis && cd .qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/EN/QgisInstallerEN.OTL && tput reset && tput bold && tput setaf 5; echo Installation files downloaded! && tput setaf 3; echo Please fill in the verification code in 3 seconds to execute the script && tput sgr 0 && sleep 3
 
 #Fill in verification code and execute script
 openssl enc -d -aes-256-cbc -a -in QgisInstallerEN.OTL | sudo bash -

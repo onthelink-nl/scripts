ee521463b0a7c684424d1a930c1d0aea4910e11c
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-07-17 10:48:35 +0200

    Update OnTheLink MENU_NL.sh

--- OnTheLink MENU_NL.sh
+++ OnTheLink MENU_NL.sh
@@ -3,17 +3,19 @@
 tput reset
 selection=
 until [ "$selection" = "0" ]; do
-echo "
+tput bold && tput setaf 4; echo "
 OnTheLink MENU
+"
+tput sgr0 && tput setaf 1; echo "
 1 - installeer/herinstalleer QGIS
 0 - Sluit dit menu
 "
-    echo -n "Voer uw keuze in: "
+    tput setaf 6; echo -n "Voer uw keuze in: "
     read -r selection
     echo ""
 case $selection in
     1 ) 
-        echo "Voorbereiden om QGIS te installeren"
+        tput setaf 5; echo "Voorbereiden om QGIS te installeren"
         sudo curl -LO https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/Startup/QgisStartNL.sh
         sleep 2 ; sudo bash QgisStartNL.sh
         ;;
@@ -21,7 +23,7 @@ case $selection in
         exit
         ;;
     * ) 
-        echo "Voer alstublieft alleen de keuzes 1 of 0 in..."
+        tput setaf 3; echo "Voer alstublieft alleen de keuzes 1 of 0 in..."
         sleep 1 ; tput reset
 esac
 done

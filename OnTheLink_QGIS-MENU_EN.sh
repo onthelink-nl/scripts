2775c2bbe90ee79e73a9906d821d5db45c3149c5
Misha Opstal <onthelink.tk@gmail.com>
2019-08-11 12:46:29 +0200

    Made it look simpler

--- OnTheLink_QGIS-MENU_EN.sh
+++ OnTheLink_QGIS-MENU_EN.sh
@@ -9,9 +9,9 @@ OnTheLink QGIS-MENU
 tput sgr0 && tput setaf 45; echo "
 1 - Install/reïnstall/update QGIS
 2 - Remove QGIS and restore settings
-3 - Exit Script and install the auto remove files script (For schools and for multiple users on one account people, THIS WILL REMOVE EVERYTHING IN THE HOME DIR!!!)
+3 - Exit Script and install the auto remove files script
 4 - Exit Script normally
-5 - Install the terminal protection software (replaces the .bashrc file in the home dir, to remove please manually remove the lines after the last fi)
+5 - Install the terminal protection software
 "
     tput setaf 6; echo -n "Enter selection: "
     read -r selection

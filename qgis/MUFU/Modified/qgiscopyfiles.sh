2ef9595af9429439336a9171a245d3125a2272c1
Misha Opstal <onthelink.tk@gmail.com>
2019-08-12 16:00:55 +0200

    Update qgiscopyfiles.sh

--- qgis/MUFU/Modified/qgiscopyfiles.sh
+++ qgis/MUFU/Modified/qgiscopyfiles.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#Create directory if it doesn't exist already
 mkdir -p "/home/$USER/qgisfiles/"
-
-#Copy the files
 for i in $(find /run/user/1000/doc/by-app/org.qgis.qgis -type f); do
-    cp $i ~/qgisfiles/
+    cp $i /home/$USER/qgisfiles/
 done

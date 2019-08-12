7db70e36558937efab28d4bdfc242c2671b467be
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-08-12 13:53:46 +0200

    Update OnTheLink_QGIS-MENU_EN.sh

--- OnTheLink_QGIS-MENU_EN.sh
+++ OnTheLink_QGIS-MENU_EN.sh
@@ -15,8 +15,9 @@ tput sgr0 && tput setaf 202; echo "
 1 - Install/reïnstall/update QGIS
 2 - Remove QGIS and restore settings
 3 - Exit Script and install the auto remove files script
-4 - Exit Script normally
-5 - Install the terminal protection software
+4 - Exit Script (RUN THIS ONE AFTER THE INSTALLATION)
+5 - Exit Script normally (RUN THIS ONE IF YOU DIDN'T INSTALL STUFF)
+6 - Install the terminal protection software
 "
 tput sgr0 && tput setaf 45; echo "
 ==================================
@@ -25,7 +26,8 @@ tput sgr0 && tput setaf 45; echo "
 "
 tput sgr0 && tput setaf 202; echo "
 a - Install/reïnstall/update QGIS LATEST + Exit Script normally (1+4)
-b - Install/reïnstall/update QGIS LATEST + The terminal protection software + Exit and install the auto remove files script (FOR SCHOOLS) (1+5+3)
+b - Install/reïnstall/update QGIS LATEST + Exit and install the auto remove files script (1+3)
+c - Install/reïnstall/update QGIS LATEST + The terminal protection software + Exit and install the auto remove files script (FOR SCHOOLS) (1+5+3)
 "
     tput setaf 6; echo -n "Enter selection: "
     read -r selection
@@ -75,8 +77,8 @@ case $selection in
 		tput reset
 		exit
 		;;
-    4 ) 
-        sudo rm -rf "qgis/"
+	4 ) 
+		sudo rm -rf "qgis/"
 		sudo rm -rf "QgisStartNL.sh"
 		sudo rm -rf "OnTheLink_QGIS-MENU_NL.sh"
 		sudo rm -rf "QgisStartEN.sh"
@@ -91,8 +93,19 @@ case $selection in
 		crontab -l | { cat; echo "* * * * * /bin/bash /etc/init.d/qgiscopyfiles.sh"; } | crontab - 
 		tput reset
 		exit
-        ;;
+		;;
     5 ) 
+        	sudo rm -rf "qgis/"
+		sudo rm -rf "QgisStartNL.sh"
+		sudo rm -rf "OnTheLink_QGIS-MENU_NL.sh"
+		sudo rm -rf "QgisStartEN.sh"
+		sudo rm -rf "OnTheLink_QGIS-MENU_EN.sh"
+		sudo rm -rf "OnTheLink_QGIS-VERSIONSELECTOR_EN.sh"
+		sudo rm -rf "OnTheLink_QGIS-VERSIONSELECTOR_NL.sh"
+		tput reset
+		exit
+		;;
+    6 ) 
 		sudo curl -LOs https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/MUFU/Modified/.bashrc > /home/$USER/.bashrc
 		tput setaf 2; echo "Terminal protection script has been installed!"
 		sleep 2
@@ -107,6 +120,32 @@ case $selection in
 		exit
 		;;
 	b ) 
+		sudo rm -rf QgisStartEN.sh
+		tput setaf 5; echo "Preparing your combination..."
+		sudo curl -LO https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/MUFU/Startup/QgisStartEN.sh
+		sleep 2 ; sudo bash QgisStartEN.sh
+		sudo rm -rf "qgis/"
+		sudo rm -rf "QgisStartNL.sh"
+		sudo rm -rf "OnTheLink_QGIS-MENU_NL.sh"
+		sudo rm -rf "QgisStartEN.sh"
+		sudo rm -rf "OnTheLink_QGIS-MENU_EN.sh"
+		sudo rm -rf "OnTheLink_QGIS-VERSIONSELECTOR_EN.sh"
+		sudo rm -rf "OnTheLink_QGIS-VERSIONSELECTOR_NL.sh"
+		sudo curl -LOs https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/MUFU/Modified/qgiscopyfiles.sh
+		sudo cp -f qgiscopyfiles.sh /etc/init.d/qgiscopyfiles.sh
+		sudo curl -LOs https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/MUFU/Modified/qgisremovefiles.sh
+		sudo cp -f qgisremovefiles.sh /etc/init.d/qgisremovefiles.sh
+		sudo chmod +x /etc/init.d/qgiscopyfiles.sh
+		sudo chmod +x /etc/init.d/qgisremovefiles.sh
+		sudo rm -rf "qgiscopyfiles.sh"
+		sudo rm -rf "qgisremovefiles.sh"
+		crontab -r
+		crontab -l | { cat; echo "* * * * * /bin/bash /etc/init.d/qgiscopyfiles.sh"; } | crontab - 
+		crontab -l | { cat; echo "@reboot /bin/bash /etc/init.d/qgisremovefiles.sh"; } | crontab - 
+		tput reset
+		exit
+		;;
+	c ) 
 		sudo rm -rf QgisStartEN.sh
 		tput setaf 5; echo "Preparing your combination..."
 		sudo curl -LO https://raw.githubusercontent.com/onthelink-tk/scripts/master/qgis/MUFU/Startup/QgisStartEN.sh

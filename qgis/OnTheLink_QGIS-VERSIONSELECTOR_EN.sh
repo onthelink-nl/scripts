8214196c23a1097410848d1c1aeec92ffe1f18b2
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-09-17 10:05:09 +0200

    Update OnTheLink_QGIS-VERSIONSELECTOR_EN.sh

--- qgis/OnTheLink_QGIS-VERSIONSELECTOR_EN.sh
+++ qgis/OnTheLink_QGIS-VERSIONSELECTOR_EN.sh
@@ -7,8 +7,9 @@ tput bold && tput setaf 46; echo "
 OnTheLink QGIS-VERSIONSELECTOR
 "
 tput sgr0 && tput setaf 45; echo "
-1 - Install update MUFU
-2 - Exit to the QGIS-MENU
+1 - Install update "LATEST"
+2 - Install update "3.6"
+3 - Exit to the QGIS-MENU
 "
     tput setaf 6; echo -n "Enter selection: "
     read -r selection
@@ -21,11 +22,17 @@ case $selection in
         sleep 2 ; sudo bash QgisStartEN.sh
         ;;
 	2 ) 
+		sudo rm -rf /home/$USER/QgisStart3.6EN.sh
+		tput setaf 5; echo "Preparing to install QGIS with update 3.6"
+        sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/master/qgis/MUFU/Startup/QgisStart3.6EN.sh
+        sleep 2 ; sudo bash QgisStart3.6EN.sh
+        ;;
+	3 ) 
 		tput reset
 		exit
 		;;
     * ) 
-        tput setaf 202; echo "Please enter choice 1 or 2..."
+        tput setaf 202; echo "Please enter choice 1, 2 or 3..."
 		sleep 1 ; tput reset
 esac
 done

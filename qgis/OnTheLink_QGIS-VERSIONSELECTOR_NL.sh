5dc0277d41c8a68ddc1a518dc664455b957c5368
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-09-17 10:15:46 +0200

    Update OnTheLink_QGIS-VERSIONSELECTOR_NL.sh

--- qgis/OnTheLink_QGIS-VERSIONSELECTOR_NL.sh
+++ qgis/OnTheLink_QGIS-VERSIONSELECTOR_NL.sh
@@ -7,8 +7,9 @@ tput bold && tput setaf 46; echo "
 OnTheLink QGIS-VERSIE_KEUZEMENU
 "
 tput sgr0 && tput setaf 45; echo "
-1 - Installeer update MUFU
-2 - Sluit dit menu en ga terug naar het QGIS-MENU
+1 - Installeer update LATEST
+2 - Installeer update 3.6
+3 - Sluit dit menu en ga terug naar het QGIS-MENU
 "
     tput setaf 6; echo -n "Voer uw keuze in: "
     read -r selection
@@ -17,10 +18,15 @@ case $selection in
     1 ) 
 		sudo rm -rf /home/$USER/QgisStartNL.sh
 		tput setaf 5; echo "Voorbereiden om QGIS te installeren met update MUFU"
-        sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/master/qgis/MUFU/Startup/QgisStartNL.sh
+        sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/Testing/qgis/MUFU/Startup/QgisStartNL.sh
         sleep 2 ; sudo bash QgisStartNL.sh
         ;;
 	2 ) 
+		sudo rm -rf /home/$USER/QgisStart3.6NL.sh
+		tput setaf 5; echo "Voorbereiden om QGIS te installeren met update 3.6"
+        sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/Testing/qgis/MUFU/Startup/QgisStart3.6NL.sh
+        sleep 2 ; sudo bash QgisStart3.6NL.sh
+	3 ) 
 		tput reset
 		exit
 		;;

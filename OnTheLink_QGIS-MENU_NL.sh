1398fe92951ab0b91b25040a8a6cd3f8575ba615
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-09-18 21:47:24 +0200

    Update OnTheLink_QGIS-MENU_NL.sh

--- OnTheLink_QGIS-MENU_NL.sh
+++ OnTheLink_QGIS-MENU_NL.sh
@@ -29,9 +29,9 @@ tput setaf 45; echo "<<<<<<<
 ==================================
 "
 tput sgr0 && tput setaf 202; echo "
-a - Installeer/herinstalleer/update QGIS LATEST + Verlaat het script (1+4)
-b - Installeer/herinstalleer/update QGIS LATEST + Verlaat het script en installeer het automatisch bestanden verwijderen script (1+3)
-c - Installeer/herinstalleer/update QGIS LATEST + De terminal beveiligings software + Verlaat het script en installeer het automatisch verwijderen van bestanden script (VOOR SCHOLEN) (VERWIJDERD ALLES IN DE THUIS MAP (/home/$USER/EVERYTHING IN HERE)) (1+5+3)
+a - Installeer/herinstalleer/update QGIS + Verlaat het script (1+4)
+b - Installeer/herinstalleer/update QGIS + Verlaat het script en installeer het automatisch bestanden verwijderen script (1+3)
+c - Installeer/herinstalleer/update QGIS + De terminal beveiligings software + Verlaat het script en installeer het automatisch verwijderen van bestanden script (VOOR SCHOLEN) (VERWIJDERD ALLES IN DE THUIS MAP (/home/$USER/EVERYTHING IN HERE)) (1+5+3)
 d - Herinstalleer het automatisch verwijderen van bestanden script + Het kopiëren script
 e - Herinstalleer het automatisch verwijderen van bestanden script + Het kopiëren script + De terminal beveiligings software
 f - Herinstalleer het kopiëren script (Deze zorgt dat je dingen kunt opslaan, zou automatisch geïnstalleerd moeten zijn)

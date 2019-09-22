d68fae557df69f6e1f53cdc56a5cb3529ba0566f
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-09-22 21:53:14 +0200

    Update QgisStart3.6NL.sh

--- qgis/MUFU/Startup/QgisStart3.6NL.sh
+++ qgis/MUFU/Startup/QgisStart3.6NL.sh
@@ -7,7 +7,7 @@ tput reset
 sudo rm -rf qgis
 
 #Commando's voor het doorvoeren naar de Installatie
-mkdir qgis && cd qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/Testing/qgis/MUFU/NL/QgisInstaller3.6NL.sh && tput reset && tput bold && tput setaf 5; echo Installatie bestanden gedownload! && tput setaf 3; echo De installatie zal starten over 3 seconden && tput sgr 0 && sleep 3
+mkdir qgis && cd qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/master/qgis/MUFU/NL/QgisInstaller3.6NL.sh && tput reset && tput bold && tput setaf 5; echo Installatie bestanden gedownload! && tput setaf 3; echo De installatie zal starten over 3 seconden && tput sgr 0 && sleep 3
 
 #DIT IS HET DOORVERWIJSSCRIPT VOOR QGIS
 sudo bash QgisInstaller3.6NL.sh

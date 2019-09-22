81043c9de64c603455585ca52e438c4df534f0a5
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-09-22 20:45:02 +0200

    Update OnTheLink_QGIS-VERSIONSELECTOR_EN.sh

--- qgis/OnTheLink_QGIS-VERSIONSELECTOR_EN.sh
+++ qgis/OnTheLink_QGIS-VERSIONSELECTOR_EN.sh
@@ -21,18 +21,21 @@ case $selection in
 		tput setaf 5; echo "Preparing to install QGIS with update LATEST"
         sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/Testing/qgis/MUFU/Startup/QgisStartEN.sh
         sleep 2 ; sudo bash QgisStartEN.sh
+	tput reset
         ;;
 	2 ) 
 		sudo rm -rf /home/$USER/QgisStart3.6EN.sh
 		tput setaf 5; echo "Preparing to install QGIS with update 3.6"
         sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/Testing/qgis/MUFU/Startup/QgisStart3.6EN.sh
         sleep 2 ; sudo bash QgisStart3.6EN.sh
+	tput reset
         ;;
 	3 ) 
 		tput reset
 		sudo flatpak update
-		echo "Updated Flatpak and it's apps"
+		tput setaf 2; echo "Updated Flatpak and it's apps"
 		sleep 3
+		tput reset
 		;;
 	4 ) 
 		tput reset

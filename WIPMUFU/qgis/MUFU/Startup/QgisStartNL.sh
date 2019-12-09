d929237bada9b552175dc33d3714017161d759f8
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-12-09 10:32:31 +0100

    Update QgisStartNL.sh

--- WIPMUFU/qgis/MUFU/Startup/QgisStartNL.sh
+++ WIPMUFU/qgis/MUFU/Startup/QgisStartNL.sh
@@ -3,11 +3,8 @@
 #Terminal leegmaken voordat het script uitvoert
 tput reset
 
-#Oudere mappen verwijderen (indien gecreëerd)
-sudo rm -rf qgis
-
 #Commando's voor het doorvoeren naar de Installatie
-mkdir qgis && cd qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/master/MUFU/qgis/MUFU/NL/QgisInstallerNL.sh && tput reset && tput bold && tput setaf 5; echo Installatie bestanden gedownload! && tput setaf 3; echo De installatie zal starten over 3 seconden && tput sgr 0 && sleep 3
+tput bold && tput setaf 3; echo De installatie zal starten over 3 seconden && tput sgr 0 && sleep 3
 
 #DIT IS HET DOORVERWIJSSCRIPT VOOR QGIS
-sudo bash QgisInstallerNL.sh
+sudo bash qgis/MUFU/NL/QgisInstallerNL.sh

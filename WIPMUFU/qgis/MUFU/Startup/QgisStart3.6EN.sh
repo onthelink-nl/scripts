afc3c9f7e7a755a40b8b3f1b4d2cd7192be5df97
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-12-09 10:35:55 +0100

    Update QgisStart3.6EN.sh

--- WIPMUFU/qgis/MUFU/Startup/QgisStart3.6EN.sh
+++ WIPMUFU/qgis/MUFU/Startup/QgisStart3.6EN.sh
@@ -3,11 +3,8 @@
 #Clear terminal before execution of the script
 tput reset
 
-#Removing older folders (if created)
-sudo rm -rf qgis
-
-#Download the script
-mkdir qgis && cd qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/master/MUFU/qgis/MUFU/EN/QgisInstaller3.6EN.sh && tput reset && tput bold && tput setaf 5; echo Installation files downloaded! && tput setaf 3; echo The script will start in 3 seconds && tput sgr 0 && sleep 3
+#WaitingForStartScript
+tput bold && tput setaf 3; echo The script will start in 3 seconds && tput sgr 0 && sleep 3
 
 #Execute the script
-sudo bash QgisInstaller3.6EN.sh
+sudo bash qgis/MUFU/EN/QgisInstaller3.6EN.sh

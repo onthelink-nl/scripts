cd9b4d8cf57bb71d431f4d0af3f10e36035b5bd6
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2019-12-10 10:40:54 +0100

    Update OnTheLink_QGIS-MENU_EN.sh

--- OnTheLink_QGIS-MENU_EN.sh
+++ OnTheLink_QGIS-MENU_EN.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#Clear terminal before execution of the script
+#Clear the Terminal
 tput reset
 
-#Download the script
+#Downloading script
 mkdir qgis && cd qgis && sudo curl -LO https://raw.githubusercontent.com/onthelink-nl/scripts/master/MUFU/OnTheLink_QGIS-MENU_EN.sh && tput reset && tput bold && tput setaf 5; echo Installation files downloaded! && tput setaf 3; echo The script will start in 3 seconds && tput sgr 0 && sleep 3
 
 #Execute the script
-sudo bash OnTheLink_QGIS-MENU_EN.sh
+bash OnTheLink_QGIS-MENU_EN.sh
+
+#Remove files at end of execution
+cd .. && sudo rm -rf qgis && sudo rm -rf OnTheLink_QGIS-MENU_EN.sh

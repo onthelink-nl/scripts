088c5a1b8c38076941977db38736fafc633886b7
Misha Opstal <onthelink.tk@gmail.com>
2020-01-12 23:18:07 +0100

    Update OnTheLink_JAVA-MENUWIP.sh

--- Java Unofficial Installer (Debian)/OnTheLink_JAVA-MENUWIP.sh
+++ Java Unofficial Installer (Debian)/OnTheLink_JAVA-MENUWIP.sh
@@ -27,63 +27,63 @@ tput sgr0 && tput setaf 202; echo "
     echo ""
 case $selection in
     1 ) 
-		sudo rm -rf javainstaller10.sh
+		sudo rm -rf javainstaller10WIP.sh
 		tput setaf 5; echo "Preparing the Java 10.0.2 installation..."
         sudo curl -LO "https://raw.githubusercontent.com/onthelink-nl/scripts/master/Java%20Unofficial%20Installer%20(Debian)/Java%2010.0.2/javainstaller10WIP.sh"
-        sleep 2 ; bash javainstaller10.sh
+        sleep 2 ; bash javainstaller10WIP.sh
 		sudo rm -rf /home/$USER/javainstaller*
-		sudo rm -rf "OnTheLink_JAVA-MENU.sh"
+		sudo rm -rf "OnTheLink_JAVA-MENUWIP.sh"
 		tput reset
 		tput clear
 		exit
         ;;
 	2 ) 
-		sudo rm -rf javainstaller9.sh
+		sudo rm -rf javainstaller9WIP.sh
 		tput setaf 5; echo "Preparing the Java 9.0.4 installation..."
         sudo curl -LO "https://raw.githubusercontent.com/onthelink-nl/scripts/master/Java%20Unofficial%20Installer%20(Debian)/Java%209.0.4/javainstaller9WIP.sh"
-        sleep 2 ; bash javainstaller9.sh
+        sleep 2 ; bash javainstaller9WIP.sh
 		sudo rm -rf /home/$USER/javainstaller*
-		sudo rm -rf "OnTheLink_JAVA-MENU.sh"
+		sudo rm -rf "OnTheLink_JAVA-MENUWIP.sh"
 		tput reset
 		tput clear
 		exit
         ;;
 	3 ) 
-		sudo rm -rf javainstaller8.sh
+		sudo rm -rf javainstaller8WIP.sh
 		tput setaf 5; echo "Preparing the Java 8u231 installation..."
         sudo curl -LO "https://raw.githubusercontent.com/onthelink-nl/scripts/master/Java%20Unofficial%20Installer%20(Debian)/Java%208u231/javainstaller8WIP.sh"
-        sleep 2 ; bash javainstaller8.sh
+        sleep 2 ; bash javainstaller8WIP.sh
 		sudo rm -rf /home/$USER/javainstaller*
-		sudo rm -rf "OnTheLink_JAVA-MENU.sh"
+		sudo rm -rf "OnTheLink_JAVA-MENUWIP.sh"
 		tput reset
 		tput clear
 		exit
         ;;
 	4 ) 
-		sudo rm -rf javainstaller7.sh
+		sudo rm -rf javainstaller7WIP.sh
 		tput setaf 5; echo "Preparing the Java 7u80 installation..."
         sudo curl -LO "https://raw.githubusercontent.com/onthelink-nl/scripts/master/Java%20Unofficial%20Installer%20(Debian)/Java%207u80/javainstaller7WIP.sh"
-        sleep 2 ; bash javainstaller7.sh
+        sleep 2 ; bash javainstaller7WIP.sh
 		sudo rm -rf /home/$USER/javainstaller*
-		sudo rm -rf "OnTheLink_JAVA-MENU.sh"
+		sudo rm -rf "OnTheLink_JAVA-MENUWIP.sh"
 		tput reset
 		tput clear
 		exit
         ;;
     5 ) 
         sudo rm -rf /home/$USER/javainstaller*
-		sudo rm -rf "OnTheLink_JAVA-MENU.sh"
+		sudo rm -rf "OnTheLink_JAVA-MENUWIP.sh"
 		tput reset
 		tput clear
 		exit
 		;;
     6 ) 
-		sudo rm -rf javauninstaller.sh
+		sudo rm -rf javauninstallerWIP.sh
 		tput setaf 5; echo "Preparing Java Removal..."
 		sudo curl -LO "https://raw.githubusercontent.com/onthelink-nl/scripts/master/Java%20Unofficial%20Installer%20(Debian)/Uninstaller/javauninstallerWIP.sh"
 		sleep 2 ; bash javauninstallerWIP.sh
 		sudo rm -rf /home/$USER/javainstaller*
-		sudo rm -rf "OnTheLink_JAVA-MENU.sh"
+		sudo rm -rf "OnTheLink_JAVA-MENUWIP.sh"
 		tput reset
 		tput clear
 		exit

8450b072f9b2d1214b20b5dbad11ea20afb1f05d
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-01-29 08:17:12 +0100

    Update UNINSTALL-MENU.sh

--- Arduino Unofficial Installer (LINUX)/Uninstallers/UNINSTALL-MENU.sh
+++ Arduino Unofficial Installer (LINUX)/Uninstallers/UNINSTALL-MENU.sh
@@ -16,7 +16,7 @@ tput setaf 45; echo "<<<<<<<<
 "
 tput sgr0 && tput setaf 202; echo "
 1 - Remove Arduino 1.8.11 from your system
-2 - Go back to the ARDUINO-MENU
+2 - Exit
 "
     tput setaf 6; echo -n "Enter selection: "
     read -r selection
@@ -40,6 +40,7 @@ case $selection in
 		tput sgr0
 		tput reset
 		tput clear
+		exit
 		;;
 	* ) 
         tput setaf 202; echo "Please enter choice 1, 2, 3..."

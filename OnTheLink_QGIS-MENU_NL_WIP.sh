c90d15d0480c378365831b845cd553d74a8718de
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-03 13:56:34 +0100

    Update OnTheLink_QGIS-MENU_NL_WIP.sh

--- OnTheLink_QGIS-MENU_NL_WIP.sh
+++ OnTheLink_QGIS-MENU_NL_WIP.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Get current user
+name="$(logname)"
+
+#Get current path (Required for completion removal of install files)
+STARTDIR="$(pwd)"
+
 #Terminal legen
 tput reset
 tput clear
@@ -9,23 +15,89 @@ tput sgr0
 sudo rm -rf /home/"$name"/qgis
 
 #Script Downloaden
-mkdir qgis
-cd qgis
+mkdir /home/"$name"/qgis
+cd /home/"$name"/qgis
 tput reset
 tput clear
 tput sgr0
 tput bold
 tput setaf 5
-echo "Installatie bestanden gedownload!"
+echo "Alles is ingesteld, de installatie kan voortzetten!"
 tput setaf 3
 echo "Het script zal starten over 3 seconden"
 tput sgr0
 sleep 3
 
 #Script uitvoeren
-bash OnTheLink_QGIS-MENU_NL.sh
+MACHINE_VERSION=`cat /etc/debian_version`
+case $MACHINE_VERSION in
+9|9.1|9.2|9.3|9.4|9.5|9.6|9.7|9.8|9.9)
+   # Stretch stuff here
 
-#Verwijder bestanden na uitvoering script
+##STRETCH CHECK VOLTOOID
+tput setaf 2
+echo "STRETCH GEVONDEN!"
+sleep 2
+
+##Commando's nadat de gebruiker heeft gedrukt op de willekeurige toets
+tput sgr0
+tput setaf 9
+echo "Stretch versie downloaden..."
+tput sgr0
+sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPMUFU/OnTheLink_QGIS-MENU_NL_STRETCH.sh
+bash OnTheLink_QGIS-MENU_NL_STRETCH.sh
 cd $STARTDIR
 sudo rm -rf /home/"$name"/qgis
-sudo rm -rf OnTheLink_QGIS-MENU_NL.sh
+tput reset
+tput clear
+tput sgr0
+exit
+;;
+10|10.1|10.2|10.3|10.4|10.5|10.6|10.7|10.8|10.9)
+   ## Buster stuff here
+
+##BUSTER CHECK VOLTOOID
+tput setaf 2
+echo "BUSTER GEVONDEN!"
+sleep 2
+
+##Commando's nadat de gebruiker heeft gedrukt op de willekeurige toets
+tput sgr0
+tput setaf 9
+echo "Buster versie downloaden..."
+tput sgr0
+sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPMUFU/OnTheLink_QGIS-MENU_NL_BUSTER.sh
+bash OnTheLink_QGIS-MENU_NL_BUSTER.sh
+cd $STARTDIR
+sudo rm -rf /home/"$name"/qgis
+sudo rm -rf OnTheLink_QGIS-MENU_NL_WIP.sh
+tput reset
+tput clear
+tput sgr0
+exit
+;;
+*)
+## FAILSAFE
+  tput setaf 3
+  echo "Stretch en Buster niet gevonden, uw debian versie wordt nog niet ondersteunt..."
+  sleep 1
+  tput setaf 1
+  echo "4"
+  sleep 1
+  tput setaf 1
+  echo "3"
+  sleep 1
+  tput setaf 1
+  echo "2"
+  sleep 1
+  tput setaf 1
+  echo "1"
+  cd $STARTDIR
+  sudo rm -rf /home/"$name"/qgis
+  sudo rm -rf OnTheLink_QGIS-MENU_NL_WIP.sh
+  tput reset
+  tput clear
+  tput sgr0
+  exit
+;;
+esac

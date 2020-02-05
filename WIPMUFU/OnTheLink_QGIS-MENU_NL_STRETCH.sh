b82f4f047f56131f746a87088dd917c34f0420d4
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-05 19:27:26 +0100

    Update OnTheLink_QGIS-MENU_NL_STRETCH.sh

--- WIPMUFU/OnTheLink_QGIS-MENU_NL_STRETCH.sh
+++ WIPMUFU/OnTheLink_QGIS-MENU_NL_STRETCH.sh
@@ -21,8 +21,8 @@ tput setaf 45; echo "<<<<<<<
 "
 tput sgr0 && tput setaf 202; echo "
 1 - Installeer/herinstalleer/update QGIS
-2 - Verwijder QGIS en zet alles terug naar standaardwaardes
-3 - Sluit dit menu en installeer het automatisch verwijderen van bestanden script
+2 - Verwijder QGIS en zet alles terug naar Standaardwaardes (Standaardwaardes om crontabs te verwijderen en ook onze sources.list)
+3 - Sluit dit menu en installeer het automatisch verwijderen van bestanden script 
 4 - Sluit dit menu en installeer het kopiëren script (zorgt ervoor dat de bestanden van qgis in de hoofdmap worden gezet)
 5 - Sluit dit menu normaal (KIES DEZE ALLEEN ALS U NIETS HEEFT GEÏNSTALLEERD)
 6 - Installeer de terminal beveiligings software

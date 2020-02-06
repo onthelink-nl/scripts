8eadd3ad9e19d6500ce65014c768fe768cf7ec9e
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-06 21:05:15 +0100

    Update updaterqgis.sh

--- WIPEVA/qgis/EVA/Modified/updaterqgis.sh
+++ WIPEVA/qgis/EVA/Modified/updaterqgis.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+  if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
   qgisu="/etc/init.d/qgisconfigupdater.sh"
 if [ -f "$qgisu" ]; then
 	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPEVA/qgis/EVA/Modified/qgisconfigupdater.sh
@@ -12,4 +13,9 @@ else
 	sudo rm -rf "qgisconfigupdater.sh"
 fi
 DISPLAY=:0 x-terminal-emulator -e bash /etc/init.d/qgisconfigupdater.sh
+
+else
+exit
+fi
+
 exit

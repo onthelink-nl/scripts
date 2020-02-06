b53ddf3f02839781202cbdc50bbc3af10b1f3b20
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-06 11:06:09 +0100

    Create OnTheLink_QGIS-MENU_EN.sh

---
+++ OnTheLink_QGIS-MENU_EN.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#Get current user
+name="$(logname)"
+
+STARTDIR="$(pwd)"
+cd /home/"$name"/
+
+tput clear
+selection=
+until [ "$selection" = "4" ]; do
+tput bold && tput setaf 46; echo "
+OnTheLink QGIS-UPDATESELECTOR | Version: 3.1 STABLE
+"
+tput sgr0 && tput setaf 45; echo -n "
+==================================
+>>>>>>>"
+tput setaf 191; echo -n "OPTIONS"
+tput setaf 45; echo "<<<<<<<
+==================================
+"
+tput sgr0 && tput setaf 202; echo "
+1 - LATEST (EVA)
+2 - MUFU (ONLY WORKS ON STRETCH (HAS NO STRETCH CHECKS!!))
+3 - Exit
+"
+    tput setaf 6; echo -n "Enter selection: "
+    read -r selection
+    echo ""
+case $selection in
+    1 ) 
+		sudo rm -rf OnTheLink_QGIS-MENU_EN_EVA.sh
+		tput setaf 5
+		echo "Preparing the QGIS-MENU (EVA)..."
+        sudo curl -LOs https://github.com/onthelink-nl/scripts/raw/master/OnTheLink_QGIS-MENU_EN_EVA.sh
+        sleep 2
+		bash OnTheLink_QGIS-MENU_EN_EVA.sh
+        ;;
+	2 ) 
+		sudo rm -rf OnTheLink_QGIS-MENU_EN_MUFU.sh
+		tput setaf 5
+		echo "Preparing the QGIS-MENU (MUFU)..."
+		sudo curl -LOs https://github.com/onthelink-nl/scripts/raw/master/OnTheLink_QGIS-MENU_EN_MUFU.sh
+		sleep 2
+		bash OnTheLink_QGIS-MENU_EN_MUFU.sh
+		;;
+  3 ) 
+        sudo rm -rf OnTheLink_QGIS-MENU_EN.sh
+        exit
+        ;;
+	* ) 
+        tput setaf 202
+		echo "Please only enter option 1 or 2..."
+		sleep 1
+		tput reset
+		;;
+esac
+done

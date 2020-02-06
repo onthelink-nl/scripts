acb52a57fa2e46656c67994a411060e87825d87f
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-06 20:34:06 +0100

    Update updaterqgis.sh

--- WIPEVA/qgis/EVA/Modified/updaterqgis.sh
+++ WIPEVA/qgis/EVA/Modified/updaterqgis.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+  qgisu="/etc/init.d/qgisconfigupdater.sh"
+if [ -f "$qgisu" ]; then
+	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPEVA/qgis/EVA/Modified/qgisconfigupdater.sh
+	sudo cp -f qgisconfigupdater.sh /etc/init.d/qgisconfigupdater.sh
+	sudo chmod +x /etc/init.d/qgisconfigupdater.sh
+	sudo rm -rf "qgisconfigupdater.sh"
+else
+	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPEVA/qgis/EVA/Modified/qgisconfigupdater.sh
+	sudo cp -f qgisconfigupdater.sh /etc/init.d/qgisconfigupdater.sh
+	sudo chmod +x /etc/init.d/qgisconfigupdater.sh
+	sudo rm -rf "qgisconfigupdater.sh"
+fi
 DISPLAY=:0 xterm +u8 -title UPDATER -fg yellow -bg black -e bash -c "while true; do bash /etc/init.d/qgisconfigupdater.sh; done"
 exit

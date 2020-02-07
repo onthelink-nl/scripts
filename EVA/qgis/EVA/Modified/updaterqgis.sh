80168fd43bd01943f186d18dff68ac8b41e756d7
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-07 09:23:29 +0100

    Update updaterqgis.sh

--- EVA/qgis/EVA/Modified/updaterqgis.sh
+++ EVA/qgis/EVA/Modified/updaterqgis.sh
@@ -2,12 +2,12 @@
   if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
   qgisu="/etc/init.d/qgisconfigupdater.sh"
 if [ -f "$qgisu" ]; then
-	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPEVA/qgis/EVA/Modified/qgisconfigupdater.sh
+	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Modified/qgisconfigupdater.sh
 	sudo cp -f qgisconfigupdater.sh /etc/init.d/qgisconfigupdater.sh
 	sudo chmod +x /etc/init.d/qgisconfigupdater.sh
 	sudo rm -rf "qgisconfigupdater.sh"
 else
-	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/WIPEVA/qgis/EVA/Modified/qgisconfigupdater.sh
+	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Modified/qgisconfigupdater.sh
 	sudo cp -f qgisconfigupdater.sh /etc/init.d/qgisconfigupdater.sh
 	sudo chmod +x /etc/init.d/qgisconfigupdater.sh
 	sudo rm -rf "qgisconfigupdater.sh"

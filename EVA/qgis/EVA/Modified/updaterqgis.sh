9a56b6c96ecb52d500b1b88611a45b9e25f91b89
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-02-10 14:01:29 +0100

    Update updaterqgis.sh

--- EVA/qgis/EVA/Modified/updaterqgis.sh
+++ EVA/qgis/EVA/Modified/updaterqgis.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
   if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
   qgisu="/etc/init.d/qgisconfigupdater.sh"
+  STARTDIR="$(pwd)"
 if [ -f "$qgisu" ]; then
+	sudo rm -rf updaterfiles/
+	sudo mkdir updaterfiles/
+	cd updaterfiles/
 	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Modified/qgisconfigupdater.sh
 	sudo cp -f qgisconfigupdater.sh /etc/init.d/qgisconfigupdater.sh
 	sudo chmod +x /etc/init.d/qgisconfigupdater.sh
 	sudo rm -rf "qgisconfigupdater.sh"
+	cd $STARTDIR
+	sudo rm -rf updaterfiles/
 else
+	sudo rm -rf updaterfiles/
+	sudo mkdir updaterfiles/
+	cd updaterfiles/
 	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Modified/qgisconfigupdater.sh
 	sudo cp -f qgisconfigupdater.sh /etc/init.d/qgisconfigupdater.sh
 	sudo chmod +x /etc/init.d/qgisconfigupdater.sh
 	sudo rm -rf "qgisconfigupdater.sh"
+	cd $STARTDIR
+	sudo rm -rf updaterfiles/
 fi
 DISPLAY=:0 x-terminal-emulator -e bash /etc/init.d/qgisconfigupdater.sh
 

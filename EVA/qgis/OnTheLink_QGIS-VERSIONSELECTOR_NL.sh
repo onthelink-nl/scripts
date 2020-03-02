b7c06a87632aa556d414cabc222070fa092015fc
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-03-02 21:07:06 +0100

    Update OnTheLink_QGIS-VERSIONSELECTOR_NL.sh

--- EVA/qgis/OnTheLink_QGIS-VERSIONSELECTOR_NL.sh
+++ EVA/qgis/OnTheLink_QGIS-VERSIONSELECTOR_NL.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#MESSAGES
+COLUMNS=$(tput cols)
+echoa="OnTheLink QGIS-VERSIE_KEUZEMENU"
+echob="1 - Installeer update "LATEST""
+echoba="2 - Installeer update "3.6""
+echobb="3 - NORMALE FLATPAK UPDATER"
+echobc="4 - Ga terug naar het QGIS-MENU"
+echoc="echo -n "Voer uw keuze in: ""
+echod="Voorbereiden om QGIS te installeren met update LATEST"
+echoe="Voorbereiden om QGIS te installeren met update 3.6"
+echof="Updates voor Flatpak en zijn apps zijn voltooid"
+echog="Voer alstublieft alleen de keuzes 1, 2, 3 of 4 in..."
+echoz=""
+
 #Get current user
 name="$(logname)"
 
@@ -10,23 +24,29 @@ tput reset
 tput clear
 selection=
 until [ "$selection" = "4" ]; do
-tput bold && tput setaf 46; echo "
-OnTheLink QGIS-VERSIE_KEUZEMENU
-"
-tput sgr0 && tput setaf 45; echo "
-1 - Installeer update "LATEST"
-2 - Installeer update "3.6"
-3 - NORMALE FLATPAK UPDATER (UPDATE ALLE FLATPAK GEÏNSTALLEERDE APPS)
-4 - Sluit dit menu en ga terug naar het QGIS-MENU
-"
-    tput setaf 6; echo -n "Voer uw keuze in: "
-    read -r selection
-    echo ""
+tput bold
+tput setaf 46
+#echoa
+printf "%*s\n" $(((${#echoa}+$COLUMNS)/2)) "$echoa"
+tput sgr0
+tput setaf 45
+#echob
+printf "%*s\n" $(((${#echob}+$COLUMNS)/2)) "$echob"
+printf "%*s\n" $(((${#echoba}+$COLUMNS)/2)) "$echoba"
+printf "%*s\n" $(((${#echobb}+$COLUMNS)/2)) "$echobb"
+printf "%*s\n" $(((${#echobc}+$COLUMNS)/2)) "$echobc"
+tput setaf 6
+#echoc
+printf "%*s\n" $(((${#echoc}+$COLUMNS)/2)) "$echoc"
+read -r selection
+#echoz
+printf "%*s\n" $(((${#echoz}+$COLUMNS)/2)) "$echoz"
 case $selection in
     1 ) 
 		sudo rm -rf /home/$USER/QgisStartNL.sh 2> /dev/null | exec 1> /dev/tty
 		tput setaf 5
-		echo "Voorbereiden om QGIS te installeren met update LATEST"
+		#echod
+		printf "%*s\n" $(((${#echod}+$COLUMNS)/2)) "$echod"
         sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Startup/QgisStartNL.sh
         sleep 2
 		sudo bash QgisStartNL.sh
@@ -38,7 +58,8 @@ case $selection in
 	2 ) 
 		sudo rm -rf /home/$USER/QgisStart3.6NL.sh 2> /dev/null | exec 1> /dev/tty
 		tput setaf 5
-		echo "Voorbereiden om QGIS te installeren met update 3.6"
+		#echoe
+		printf "%*s\n" $(((${#echoe}+$COLUMNS)/2)) "$echoe"
         sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Startup/QgisStart3.6NL.sh
         sleep 2
 		sudo bash QgisStart3.6NL.sh
@@ -52,7 +73,9 @@ case $selection in
 		tput clear
 		tput sgr0
 		sudo flatpak update
-		tput setaf 2; echo "Updates voor Flatpak en zijn apps zijn voltooid"
+		tput setaf 2
+		#echof
+		printf "%*s\n" $(((${#echof}+$COLUMNS)/2)) "$echof"
 		sleep 3
 		tput reset
 		tput clear
@@ -68,7 +91,8 @@ case $selection in
 		;;
     * ) 
         tput setaf 202
-		echo "Voer alstublieft alleen de keuzes 1, 2 of 3 in..."
+		#echog
+		printf "%*s\n" $(((${#echog}+$COLUMNS)/2)) "$echog"
 		sleep 1
 		tput reset
 		tput clear

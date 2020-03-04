48b40016b794e2c60781a7ae393c79a2057fc1df
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-03-04 23:08:30 +0100

    Update Removal_EN.sh

--- EVA/qgis/Removal_EN.sh
+++ EVA/qgis/Removal_EN.sh
@@ -3,6 +3,13 @@
 STARTDIR="$(pwd)"
 cd /home/$USER/
 
+phases=( 
+    'REMOVING QGIS (org.qgis.qgis)...'
+    'REMOVING FLATPAK REMOTES...'
+    'REMOVING UNUSED FLATPAK APPS...'
+    'RESTORING DEFAULT SETTINGS...'
+)
+
 tput reset
 tput clear
 selection=
@@ -21,6 +28,18 @@ tput sgr0 && tput setaf 45; echo "
     echo ""
 case $selection in
     1 ) 
+		for i in $(seq 1 100); do  
+    sleep 0.1
+
+    if [ $i -eq 100 ]; then
+        echo -e "XXX\n100\nDone!\nXXX"
+    elif [ $(($i % 25)) -eq 0 ]; then
+        let "phase = $i / 25"
+        echo -e "XXX\n$i\n${phases[phase]}\nXXX"
+    else
+        echo $i
+    fi 
+done | dialog --title 'REMOVING QGIS + FLATPAK' --gauge "${phases[0]}" 6 60 0
 		tput reset
 		tput clear
 		tput setaf 1

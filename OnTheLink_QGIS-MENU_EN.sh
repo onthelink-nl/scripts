88280538b64d96cbce7830621962fca293577795
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-03-04 20:58:38 +0100

    Update OnTheLink_QGIS-MENU_EN.sh

--- OnTheLink_QGIS-MENU_EN.sh
+++ OnTheLink_QGIS-MENU_EN.sh
@@ -1,64 +1,53 @@
 #!/bin/bash
 
-#Get current user
-name="$(logname)"
+if [[ $EUID -ne 1000 ]]; then
+   tput setaf 1
+   echo "This script cannot be started using the "sudo" command"
+   sleep 3
+   tput reset
+   tput clear
+   tput sgr0
+   exit 1
+fi
 
-STARTDIR="$(pwd)"
-cd /home/"$name"/
+sudo apt-get --yes --assume-yes install dialog 2> /dev/null | exec 1> /dev/tty
 
-tput reset
-tput clear
-selection=
-until [ "$selection" = "3" ]; do
-tput bold && tput setaf 46; echo "
-OnTheLink QGIS-UPDATESELECTOR | Version: 3.1 STABLE
-"
-tput sgr0 && tput setaf 45; echo -n "
-=====================
->>>>>>>"
-tput setaf 191; echo -n "OPTIONS"
-tput setaf 45; echo "<<<<<<<
-=====================
-"
-tput sgr0 && tput setaf 202; echo "
-1 - LATEST (EVA)
-2 - MUFU (ONLY WORKS ON STRETCH (HAS NO STRETCH CHECKS!!))
-3 - Exit
-"
-    tput setaf 6; echo -n "Enter selection: "
-    read -r selection
-    echo ""
-case $selection in
-    1 ) 
-		sudo rm -rf OnTheLink_QGIS-MENU_EN_EVA.sh
-		tput setaf 5
-		echo "Preparing the QGIS-MENU (EVA)..."
-        sudo curl -LOs https://github.com/onthelink-nl/scripts/raw/master/OnTheLink_QGIS-MENU_EN_EVA.sh
-        sleep 2
-		bash OnTheLink_QGIS-MENU_EN_EVA.sh
-		exit
-        ;;
-	2 ) 
-		sudo rm -rf OnTheLink_QGIS-MENU_EN_MUFU.sh
-		tput setaf 5
-		echo "Preparing the QGIS-MENU (MUFU)..."
-		sudo curl -LOs https://github.com/onthelink-nl/scripts/raw/master/OnTheLink_QGIS-MENU_EN_MUFU.sh
-		sleep 2
-		bash OnTheLink_QGIS-MENU_EN_MUFU.sh
-		exit
-		;;
-  3 ) 
-        tput reset
-	tput clear
-	tput sgr0
-	sudo rm -rf OnTheLink_QGIS-MENU_EN.sh
-        exit
-        ;;
-	* ) 
-        tput setaf 202
-		echo "Please only enter option 1, 2 or 3..."
-		sleep 1
-		tput reset
-		;;
+export NCURSES_NO_UTF8_ACS=1
+dialog --title "Update " \
+--backtitle "QGIS Installation - Created by OnTheLink" \
+--yesno "Use newest update? \nPress No for update MUFU" 0 0
+
+# Get exit status
+# 0 means user hit [yes] button.
+# 1 means user hit [no] button.
+# 255 means user hit [Esc] key.
+response=$?
+case $response in
+   0) 
+      tput setaf 2
+      echo "Update EVA will be used for this installation!"
+      sudo rm -rf OnTheLink_QGIS-MENU_EN_EVA.sh
+      sudo curl -LOs https://github.com/onthelink-nl/scripts/raw/master/OnTheLink_QGIS-MENU_EN_EVA.sh
+      sleep 2
+      bash OnTheLink_QGIS-MENU_EN_EVA.sh
+      exit
+      ;;
+   1) 
+      tput setaf 2
+      echo "Update MUFU will be used for this installation!"
+      sudo rm -rf OnTheLink_QGIS-MENU_EN_MUFU.sh
+      tput setaf 5
+      echo "Preparing the QGIS-MENU (MUFU)..."
+      sudo curl -LOs https://github.com/onthelink-nl/scripts/raw/master/OnTheLink_QGIS-MENU_EN_MUFU.sh
+      sleep 2
+      bash OnTheLink_QGIS-MENU_EN_MUFU.sh
+      exit
+      ;;
+   255) 
+      tput setaf 1
+      echo "[ESC] Button has been pressed, Installation will be aborted"
+      tput sgr0
+      tput reset
+      tput clear
+      exit 255;;
 esac
-done

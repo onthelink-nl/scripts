6e4c2622f64ef3402000f06cbb54dc85495fbe45
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-03-04 18:05:57 +0100

    Update OnTheLink_QGIS-MENU_NL_EVA.sh

--- OnTheLink_QGIS-MENU_NL_EVA.sh
+++ OnTheLink_QGIS-MENU_NL_EVA.sh
@@ -3,6 +3,9 @@
 #Get current user
 name="$(logname)"
 
+#Install Dialog
+sudo apt-get --yes --assume-yes install dialog
+
 #Get current path (Required for completion removal of install files)
 STARTDIR="$(pwd)"
 

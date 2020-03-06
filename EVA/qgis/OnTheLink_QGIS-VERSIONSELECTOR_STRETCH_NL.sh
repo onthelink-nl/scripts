1c67a03ed65f68e6dbcfc598bba0b057ed7a9ae5
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-03-06 12:30:28 +0100

    Update OnTheLink_QGIS-VERSIONSELECTOR_STRETCH_NL.sh

--- EVA/qgis/OnTheLink_QGIS-VERSIONSELECTOR_STRETCH_NL.sh
+++ EVA/qgis/OnTheLink_QGIS-VERSIONSELECTOR_STRETCH_NL.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#MESSAGES
-COLUMNS=$(tput cols)
-echoa="OnTheLink QGIS-VERSIE_KEUZEMENU"
-echob="1 - Installeer update "LATEST""
-echoba="2 - Installeer update "3.6""
-echobb="3 - NORMALE FLATPAK UPDATER"
-echobc="4 - Ga terug naar het QGIS-MENU"
-echoc="echo -n "Voer uw keuze in: ""
-echod="Voorbereiden om QGIS te installeren met update LATEST"
-echoe="Voorbereiden om QGIS te installeren met update 3.6"
-echof="Updates voor Flatpak en zijn apps zijn voltooid"
-echog="Voer alstublieft alleen de keuzes 1, 2, 3 of 4 in..."
-echoz=""
+export NCURSES_NO_UTF8_ACS=1
 
 #Get current user
 name="$(logname)"
@@ -22,84 +10,86 @@ cd /home/"$name"/
 
 tput reset
 tput clear
-selection=
-until [ "$selection" = "4" ]; do
-tput bold
-tput setaf 46
-#echoa
-printf "%*s\n" $(((${#echoa}+$COLUMNS)/2)) "$echoa"
-tput sgr0
-tput setaf 45
-#echob
-printf "%*s\n" $(((${#echob}+$COLUMNS)/2)) "$echob"
-printf "%*s\n" $(((${#echoba}+$COLUMNS)/2)) "$echoba"
-printf "%*s\n" $(((${#echobb}+$COLUMNS)/2)) "$echobb"
-printf "%*s\n" $(((${#echobc}+$COLUMNS)/2)) "$echobc"
-tput setaf 6
-#echoc
-printf "%*s\n" $(((${#echoc}+$COLUMNS)/2)) "$echoc"
-read -r selection
-#echoz
-printf "%*s\n" $(((${#echoz}+$COLUMNS)/2)) "$echoz"
+
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
+exec 3>&1
+selection=$(dialog \
+    --backtitle "QGIS Versie_keuzemenu - Created by OnTheLink" \
+    --title "Versie_keuzemenu" \
+    --clear \
+    --nocancel \
+    --ok-label "Ga verder" \
+    --cancel-label "Annuleer" \
+    --menu "OnTheLink_QGIS-VERSIE_KEUZEMENU | EVA-STRETCH | Alle Opties" $HEIGHT $WIDTH 0 \
+    "1" "Installeer update "LATEST"" \
+    "2" "Installeer update "3.6"" \
+    "3" "NORMALE FLATPAK UPDATER" \
+    "4" "Ga terug naar het QGIS-MENU" \
+    2>&1 1>&3)
+
 case $selection in
-    1 ) 
-		sudo rm -rf /home/$USER/QgisStartNL.sh 2> /dev/null | exec 1> /dev/tty
-		tput setaf 5
-		#echod
-		printf "%*s\n" $(((${#echod}+$COLUMNS)/2)) "$echod"
-        sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Startup/QgisStartNL.sh
-        sleep 2
-		sudo bash QgisStartNL.sh
+    1 )
+      sudo rm -rf /home/$USER/QgisStartNL.sh 2> /dev/null | exec 1> /dev/tty
+        	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Startup/QgisStartNL.sh
+        	sleep 2
+		bash QgisStartNL.sh
 		sudo rm -rf /home/$USER/QgisStartNL.sh 2> /dev/null | exec 1> /dev/tty
 		tput reset
 		tput clear
 		tput sgr0
-        ;;
-	2 ) 
-		sudo rm -rf /home/$USER/QgisStart3.6NL.sh 2> /dev/null | exec 1> /dev/tty
-		tput setaf 5
-		#echoe
-		printf "%*s\n" $(((${#echoe}+$COLUMNS)/2)) "$echoe"
-        sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Startup/QgisStart3.6NL.sh
-        sleep 2
-		sudo bash QgisStart3.6NL.sh
+		;;
+    2 )
+      sudo rm -rf /home/$USER/QgisStart3.6NL.sh 2> /dev/null | exec 1> /dev/tty
+        	sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/qgis/EVA/Startup/QgisStart3.6NL.sh
+        	sleep 2
+		bash QgisStart3.6NL.sh
 		sudo rm -rf /home/$USER/QgisStart3.6NL.sh 2> /dev/null | exec 1> /dev/tty
 		tput reset
 		tput clear
 		tput sgr0
-        ;;
-	3 ) 
-		tput reset
+		;;
+    3 )
+      tput reset
 		tput clear
 		tput sgr0
-		sudo flatpak update
-		tput setaf 2
-		#echof
-		printf "%*s\n" $(((${#echof}+$COLUMNS)/2)) "$echof"
+		flatpak --user update
 		sleep 3
 		tput reset
 		tput clear
 		tput sgr0
 		;;
-	4 ) 
-		tput reset
+    4 )
+                tput reset
 		tput clear
 		tput sgr0
 		cd $STARTDIR
 		sudo rm -rf OnTheLink_QGIS-VERSIONSELECTOR_STRETCH_NL.sh 2> /dev/null | exec 1> /dev/tty
-		sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/OnTheLink_QGIS-MENU_NL_BUSTER.sh
-		bash OnTheLink_QGIS-MENU_STRETCH_NL_BUSTER.sh
+		sudo curl -LOs https://raw.githubusercontent.com/onthelink-nl/scripts/master/EVA/OnTheLink_QGIS-MENU_NL_STRETCH.sh
+		bash OnTheLink_QGIS-MENU_NL_STRETCH.sh
 		exit
 		;;
-    * ) 
-        tput setaf 202
-		#echog
-		printf "%*s\n" $(((${#echog}+$COLUMNS)/2)) "$echog"
-		sleep 1
-		tput reset
-		tput clear
-		tput sgr0
-		;;
+  esac
+  
+response=$?
+case $response in
+   1) 
+      cd $STARTDIR
+      sudo rm -rf OnTheLink_QGIS-VERSIONSELECTOR_STRETCH_NL.sh
+      tput reset
+      tput clear
+      tput sgr0
+      exit
+      ;;
+   255) 
+      tput setaf 1
+      cd $STARTDIR
+      sudo rm -rf OnTheLink_QGIS-VERSIONSELECTOR_STRETCH_NL.sh
+      echo "[ESC] Knop is ingedrukt, Installatie wordt afgebroken"
+      tput sgr0
+      tput reset
+      tput clear
+      exit 255;;
 esac
-done
-

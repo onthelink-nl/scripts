ee1927b30c9ec781ca937bf443749de866469005
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-04-03 01:42:56 +0200

    Delete protectionsoftware.sh

--- EVA/qgis/EVA/Modified/protectionsoftware.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export HOME=$(bash <<< "echo ~${SUDO_USER:-}")
-cd $HOME
-sudo chmod 777 $HOME/.bashrc
-sudo rm -rf .bashrc
-sudo ln -s /etc/init.d/.bashrc $HOME/.bashrc
-sudo chmod 444 $HOME/.bashrc

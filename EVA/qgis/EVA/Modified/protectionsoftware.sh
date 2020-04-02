053b98bb0ec3322fc3754d5cac5fa39c03761ef9
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-04-02 14:06:24 +0200

    Update protectionsoftware.sh

--- EVA/qgis/EVA/Modified/protectionsoftware.sh
+++ EVA/qgis/EVA/Modified/protectionsoftware.sh
@@ -2,5 +2,7 @@
 
 export HOME=$(bash <<< "echo ~${SUDO_USER:-}")
 cd $HOME
+sudo chmod 777 $HOME/.bashrc
 sudo rm -rf .bashrc
 sudo ln -s /etc/init.d/.bashrc $HOME/.bashrc
+sudo chmod 444 $HOME/.bashrc

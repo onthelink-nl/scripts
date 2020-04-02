44a2057b27872c040b214f99a8f9c5863f4ce500
Misha Opstal <52963380+OnTheLink@users.noreply.github.com>
2020-04-02 13:34:59 +0200

    Delete qgiscopyfiles.sh

--- EVA/qgis/EVA/Modified/qgiscopyfiles.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for i in $(find /run/user/1000/doc/by-app/org.qgis.qgis -type f); do
-    cp $i ~/
-done
-
-export HOME=$(bash <<< "echo ~${SUDO_USER:-}")
-cd $HOME
-sudo rm -rf *.qgs~
